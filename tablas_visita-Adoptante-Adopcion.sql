528ddb321114540c05030c0e8b99a28ca903bdaf
Victoria <martinez15zz@live.com.mx>
2018-11-26 11:14:33 -0600

    Creaciond e la BD con todas las tablas

--- tablas_visita-Adoptante-Adopcion.sql
+++ tablas_visita-Adoptante-Adopcion.sql
@@ -1,28 +1,3 @@
-CREATE DATABASE Proyecto_DLP
-GO
-
-CREATE TABLE Perro
-(
-	IdPerro INT IDENTITY(1,1) PRIMARY KEY,
-	Nombre VARCHAR(30),
-	FechaIngreso DATE,
-	Edad INT,
-	Raza VARCHAR(20),
-	Tamaño DECIMAL(7,2),
-	Esterilizado BIT
-)
-GO
-
-CREATE TABLE Vacuna
-(
-	IdPerro INT,
-	Fecha DATE,
-	TipoVacuna VARCHAR(30),
-
-	FOREIGN KEY(IdPerro) REFERENCES Perro(IdPerro)
-)
-
-
 CREATE TABLE Adopción 
 (
 IdAdopcion int IDENTITY(1,1) PRIMARY KEY NOT NULL,
